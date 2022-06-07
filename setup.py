4ba22227e892116edb28b4071b52ff5d7ab9425e
Alex Vidchinkin <ulmen.ua@gmail.com>
2022-06-07 17:51:25 +0200

    Move back tap-kit dependency

--- setup.py
+++ setup.py
@@ -12,9 +12,12 @@ setup(
     install_requires=[
         "singer-python==5.2.0",
         "requests==2.18.4",
+        "tap-kit @ git+https://github.com/dmzobel/tap-kit.git@master",
         "pendulum==1.2.0"
     ],
-    dependency_links=[],
+    dependency_links=[
+        "https://github.com/dmzobel/tap-kit/tarball/master#egg=tap-kit-0.1.1"
+    ],
     entry_points="""
     [console_scripts]
     tap-contentful=tap_contentful:main
